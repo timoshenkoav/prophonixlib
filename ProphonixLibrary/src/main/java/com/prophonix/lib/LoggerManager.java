a6bfba49a6e9b707d84388a531989435a41002dd
Alexandr Timoshenko <atimoshenko@prophonix.com>
2014-03-23 00:36:14 +0200

    added files

---
+++ ProphonixLibrary/src/main/java/com/prophonix/lib/LoggerManager.java
@@ -0,0 +1,10 @@
+package com.prophonix.lib;
+
+/**
+ * Created by Alexandr Timoshenko on 3/23/14.
+ */
+public final class LoggerManager {
+    public static void log(String format,Object ... params){
+
+    }
+}
